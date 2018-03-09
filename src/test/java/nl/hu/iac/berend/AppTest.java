ab63f6a8cfd8e98f80d6948d2483766acfd3206a
berend.wilkens <berend.wilkens@hu.nl>
2018-03-09 10:45:12 +0100

    test commit

--- src/test/java/nl/hu/iac/berend/AppTest.java
+++ src/test/java/nl/hu/iac/berend/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest extends TestCase {
 	public AppTest(String testName) {
 		super(testName);
 		double result = App.calculateSimpleInterest(10000, 5, 7);
-		Assert.assertEquals("Test failed. ", 350000.0, result);
+		Assert.assertEquals("Test failed. ", 35000.0, result);
 	}
 
 	public static Test suite() {
